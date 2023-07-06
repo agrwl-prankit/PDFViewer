d31ce3127bb096cd454db619ee2439be5d228bc1
agrwl-prankit <agrwnlprankit77@gmail.com>
2023-07-06 12:41:43 +0530

    added PdfActivity

--- app/src/main/java/com/prankit/pdfviewer/PdfActivity.java
+++ app/src/main/java/com/prankit/pdfviewer/PdfActivity.java
@@ -1,13 +1,24 @@
 package com.prankit.pdfviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class PdfActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf);
+
+        PDFView pdfView = findViewById(R.id.pdfViewer);
+        pdfView.fromAsset("projectproposal.pdf")
+                .enableDoubletap(true)
+                .fitEachPage(true)
+                .load();
     }
 }
